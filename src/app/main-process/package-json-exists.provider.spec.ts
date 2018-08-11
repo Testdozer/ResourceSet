7ea0239b45eb4aace11eafe1ffc08d3a35db21e9
Ivan Norin <ivan.norin@gmail.com>
2018-08-11 14:40:03 +0300

    The package json exist provider has been tested.

---
+++ src/app/main-process/package-json-exists.provider.spec.ts
@@ -0,0 +1,58 @@
+import * as fs from "fs";
+import { It, Mock } from "moq.ts";
+import * as path from "path";
+import { PackageJsonExistsProvider } from "./package-json-exists.provider";
+import { PackageJsonProvider } from "./package-json.provider";
+import { PathParentIterator } from "./path-parent.iterator";
+
+describe("Package Json Exists Provider", () => {
+
+  it("Returns true if file exist", () => {
+    const directory = "/";
+    const fileExist = true;
+
+    const sep = "/";
+    const filePath = `${directory}${sep}package.json`;
+
+    const join = new Mock<typeof path.join>()
+      .setup(instance => instance(directory, "package.json"))
+      .returns(filePath)
+      .object();
+
+    const existsSync = new Mock<typeof fs.existsSync>()
+        .setup(instance => instance(filePath))
+        .returns(fileExist)
+        .object();
+
+    const provider = new PackageJsonExistsProvider(join, existsSync);
+
+    const actual = provider.isExists(directory);
+
+    expect(actual).toBe(true);
+  });
+
+  it("Returns false if file does not exist", () => {
+    const directory = "/";
+    const fileExist = false;
+
+    const sep = "/";
+    const filePath = `${directory}${sep}package.json`;
+
+    const join = new Mock<typeof path.join>()
+      .setup(instance => instance(directory, "package.json"))
+      .returns(filePath)
+      .object();
+
+    const existsSync = new Mock<typeof fs.existsSync>()
+        .setup(instance => instance(filePath))
+        .returns(fileExist)
+        .object();
+
+    const provider = new PackageJsonExistsProvider(join, existsSync);
+
+    const actual = provider.isExists(directory);
+
+    expect(actual).toBe(false);
+  });
+
+});
