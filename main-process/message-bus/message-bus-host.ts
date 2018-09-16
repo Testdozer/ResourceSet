161aedc113dc593203152c15a94719c810704268
Radik Khisamov <radik_h@yahoo.com>
2018-09-16 17:46:51 +0300

    effect processing separated

--- main-process/message-bus/message-bus-host.ts
+++ main-process/message-bus/message-bus-host.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "injection-js";
-import { filter, map, tap } from "rxjs/operators";
+import { filter, map, tap, merge, mergeMap } from "rxjs/operators";
 import { AddHistoryItemAction } from "src/app/core.module/ipc-actions/add-history-item.action";
 import { OpenProjectAction } from "src/app/core.module/ipc-actions/open-project.action";
 import { ProjectNameProvider } from "../package-name/project-name.provider";
 import { ActionBuilder } from "./action-builder";
 import { MessageBus } from "./message.bus";
+import { forkJoin, combineLatest, of } from "rxjs";
+import { ofType } from "@ngrx/effects";
 
 @Injectable()
 export class MessageBusHost {
@@ -17,13 +19,22 @@ export class MessageBusHost {
   public start() {
     this.messageBus.messages$()
       .pipe(
-        map(({action, sender}) => ({action: this.actionBuilder.get(action), sender})),
-        filter(value => value.action.type === OpenProjectAction.type),
-        map(({action, sender}) => ({action: (action as OpenProjectAction), sender})),
-        map(({action, sender}) => ({path: action.payload.path, sender})),
-        map(({path, sender}) => ({path, name: this.projectNameProvider.get(path), sender})),
-        map(({path, name, sender}) => ({action: new AddHistoryItemAction({name, path}), sender})),
-        map(({action, sender}) => sender(action))
+        map(({action, sender}) => ({action1: this.actionBuilder.get(action), sender})),
+        mergeMap(({action1, sender}) => {
+          return combineLatest(
+            of(action1)
+              .pipe(
+                ofType<OpenProjectAction>(OpenProjectAction.type),
+                // filter(action => action.type === OpenProjectAction.type),
+                map(action => (action as OpenProjectAction)),
+                map(action => action.payload.path),
+                map(path => ({path, name: this.projectNameProvider.get(path)})),
+                map(({path, name}) => new AddHistoryItemAction({name, path}))
+              ),
+              of(sender)
+            );
+        }),
+        map(([action, sender]) => sender(action))
       )
      .subscribe( () => undefined );
   }
